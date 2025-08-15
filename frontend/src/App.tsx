14ead7513cdbacd53234dd0ba452e1a3fc0e54d3
balajisiva123 <141250784+balajisiva123@users.noreply.github.com>
2025-08-15 18:27:29 +0530

    first phasev1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import { fetchApi } from "./apiClient";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function  App(){
+  const [user,setUser]=useState<{id:string;name:string;}|null>(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+     async function loadUser(){
+      const data=await fetchApi("getUser",{id:"123"});
+      setUser(data);
+     }
+
+  return  <div>
+      <button onClick={loadUser}>Get User</button>
+      {user && <p>{user.name}</p>}
+    </div>
 }
-
-export default App
--- frontend/src/apiClient.ts
+++ frontend/src/apiClient.ts
@@ -1,1 +1,21 @@
-import {API} from "../../shared/api";
+import type {API} from "../../shared/api";
+
+type EndPoints={
+    getUser:{
+        req:API.GetUser.Request;
+        res:API.GetUser.Response;
+    }
+}
+
+export async function fetchApi<K extends keyof EndPoints>(
+    endpoint:K,
+    req:EndPoints[K]["req"]):Promise<EndPoints[K]["res"]>
+    {
+        const res=await fetch(`http://localhost:4000/api/${endpoint}`,{
+            method:"POST",
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(req)
+        });
+        return res.json();
+    }
+
